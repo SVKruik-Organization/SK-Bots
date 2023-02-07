5d779f527e49701eaebec166963bac6be166f1af
Stefan Kruik <stefan.kruik@hva.nl>
2023-02-07 22:43:19 +0100

    Server switch
    Really need a config command to auto switch

--- src/assets/config.js
+++ src/assets/config.js
@@ -2,6 +2,6 @@ exports.general = {
     name: "Stelleri",
     color: "#E4B081",
     clientId: "1071428183003500544",
-    guildId: "1022886794510999664",
-    eventChannel: "1071820918025891941"
+    guildId: "1068138763097739304",
+    eventChannel: "1072476767991906354"
 };
