0cf0cb02c9a612100f0a55ed62bb26053535baee
Stefan Kruik <stefan.kruik@hva.nl>
2023-09-14 22:02:24 +0200

    Patch

--- Stelleri/commands/ai.js
+++ Stelleri/commands/ai.js
@@ -1,6 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const { Configuration, OpenAIApi } = require('openai');
-const config = require('../assets/config.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -19,26 +17,6 @@ module.exports = {
         const actionType = interaction.options.getString('action');
         const prompt = interaction.options.getString('prompt');
 
-        const configuration = new Configuration({
-            organization: process.env.OPENAI_ORGANIZATION,
-            apiKey: process.env.OPENAI_TOKEN,
-        });
-        const openai = new OpenAIApi(configuration);
-        try {
-            const completion = await openai.createCompletion({
-                model: "text-davinci-003",
-                prompt: prompt,
-            });
-            console.log(completion.data.choices[0].text);
-        } catch (err) {
-            if (err.response.status == 401) {
-                await interaction.reply({ content: "Internal error. Try again later.", ephemeral: true });
-            } else if (err.response.data.error.type == "insufficient_quota") {
-                await interaction.reply({ content: `${config.general.name} is out of funds.`, ephemeral: false });
-            } else if (err.response.status == 500) {
-                await interaction.reply({ content: "OpenAI has a server error.", ephemeral: false });
-            }
-            console.log(err.response.data);
-        };
+        await interaction.reply({ content: "This command is currently disabled. Come back later.", ephemeral: true });
     }
 };
