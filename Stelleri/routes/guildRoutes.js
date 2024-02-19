789c850c8a399ab3af58a80317115259085269c0
Stefan Kruik <s.kruik@icloud.com>
2024-02-19 23:53:17 +0100

    New endpoint for fetching server icons

--- Stelleri/routes/guildRoutes.js
+++ Stelleri/routes/guildRoutes.js
@@ -56,4 +56,21 @@ router.put('/update/settings', jwtUtils.authenticateJWT, function (req, res) {
     } else return res.sendStatus(400);
 });
 
+router.get('/picture', jwtUtils.authenticateJWT, async function (req, res) {
+    const guilds = req.body;
+    const pictures = [];
+    for (let i = 0; i < guilds.length; i++) {
+        try {
+            const guild = await modules.client.guilds.fetch(guilds[i]);
+            if (guild) pictures.push(guild.iconURL());
+        } catch (error) {
+            if (error.status === 404) {
+                pictures.push(error.status);
+            } else return res.status(error.status).send({ message: error.message });
+        }
+    }
+    
+    res.status(200).send({ picture_urls: pictures });
+});
+
 module.exports = router;
